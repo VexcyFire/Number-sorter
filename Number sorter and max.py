26c7df305cd2debacc30c4c07c5cda3c17b9e097
VexcyFire <149859100+VexcyFire@users.noreply.github.com>
2023-11-25 17:49:04 -0700

    Add files via upload

---
+++ Number sorter and max.py
@@ -0,0 +1,12 @@
+class number():
+   file = []
+   max = max(file)
+   print(str(max))
+   file2 = []
+   sort = sorted(file2)
+   print(str(sort))       
+def number_2():        
+   if number != True:
+    return(Fail)
+ 
+Fail = 'This program failed'
